b3a3fad424e1aaaedb926a20de2eea8968612090
Greg Foreman <gfore@mindspring.com>
2020-10-19 17:36:45 -0400

    use NavLink instead of Link to apply active styles

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  NavLink,
 } from "react-router-dom";
 
 export default function App() {
@@ -12,44 +13,37 @@ export default function App() {
       <div>
         <nav>
           <ul>
+            <li><Link to="/">Home</Link></li>
             <li>
-              <Link to="/">Home</Link>
+              <NavLink
+                to="/about"
+                activeStyle={{
+                  fontWeight: "bold",
+                  color: "red"
+                }}
+              >
+                About
+              </NavLink>
             </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
+            <li><Link to={{pathname: "/users"}}>Users</Link></li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+        {/*
+          A <Switch> looks through all its children <Route> elements and renders the first
+          one whose path matches the current URL. Use a <Switch> any time you have multiple
+          routes, but you want only one of them to render at a time
+        */}
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
+          <Route path="/about"><About /></Route>
+          <Route path="/users"><Users /></Route>
+          <Route path="/"><Home /></Route>
         </Switch>
       </div>
     </Router>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
-}
+const Home = () => (<h2>Home</h2>);
+const About = () => (<h2>About</h2>);
+const Users = () => (<h2>Users</h2>);
